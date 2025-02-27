745d6094cfeecb00d4427eacaa726684840798ff
jawad486 <jawadqureshi486@gmail.com>
2025-02-27 10:58:02 -0500

    3rd commit is added

--- src/oops/Polymorphsim.java
+++ src/oops/Polymorphsim.java
@@ -2,17 +2,23 @@ package oops;
 
 public class Polymorphsim {
 	
-	int a=20;
-	int b=30;
+	static String name ="jawad";
+	static String lastName="qureshi";
 	
 	public static void main(String[] args) {
 	
 		Polymorphsim p=new Polymorphsim();
-		p.display();
+		p.display(name);
+		p.display(name, lastName);
 	}
 	
-	public void display() {
-		System.out.println("the value of a is:"+a);
+	public void display(String name) {
+		System.out.println("my name is :"+name);
+		
+	}
+	
+	public void display(String name, String lastName) {
+		System.out.println("my name is :"+name+lastName);
 		
 	}
 
